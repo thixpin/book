55d37e6020bf8800af88fd04302b4fd953c6ffb1
thixpin <thixpin@gmail.com>
2022-05-10 13:35:49 +0630

    Add pagination on book list

--- plugins/thixpin/book/components/BookList.php
+++ plugins/thixpin/book/components/BookList.php
@@ -2,6 +2,8 @@
 
 use Thixpin\Book\Models\Book;
 use Cms\Classes\ComponentBase;
+use Thixpin\Book\Models\Author;
+use Thixpin\Book\Models\Artist;
 
 /**
  * BookList Component
@@ -18,7 +20,20 @@ class BookList extends ComponentBase
 
     public function defineProperties()
     {
-        return [];
+        return [
+            'listBy' => [
+                'title' => 'List By',
+                'description' => 'Display book for a specific type',
+                'default' => '{{ :listBy }}',
+                'type' => 'string'
+            ],
+            'slug' => [
+                'title' => 'Slug of type',
+                'description' => 'The slug of type to display',
+                'default' => '{{ :slug }}',
+                'type' => 'string'
+            ],
+        ];
     }
 
     public function onRun()
@@ -28,6 +43,37 @@ class BookList extends ComponentBase
 
     protected function listBooks()
     {
-        return Book::all();
+        $listType = $this->property('listBy');
+        $slug = $this->property('slug');
+        $page = $_GET['page'] ?? 1;
+        $perPage = $_GET['perPage'] ?? 8;
+
+        switch ($listType) {
+            case 'category':
+                $books =  Book::wherehas('categories', function ($query) use ($slug) {
+                    $query->where('slug', $slug);
+                })->paginate($perPage, $page);
+                break;
+            case 'tag':
+                $books =  Book::wherehas('tags', function ($query) use ($slug) {
+                    $query->where('slug', $slug);
+                })->paginate($perPage, $page);
+                break;
+            case 'author':
+                $author = Author::where('slug', $slug)->first();
+                $authorId = $author ? $author->id : 0;
+                $books =  Book::where('author_id', $authorId)->paginate($perPage, $page);
+                break;
+            case 'artist':
+                $artist = Artist::where('slug', $slug)->first();
+                $artistId = $artist ? $artist->id : 0;
+                $books =  Book::where('artist_id', $artistId)->paginate($perPage, $page);
+                break;
+            default:
+                $books =  Book::paginate($perPage, $page);
+                break;
+        }
+        
+        return $books;
     }
 }

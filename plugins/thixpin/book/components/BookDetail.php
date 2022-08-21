819dd2b62c5ff0e4549b28b436da0dfdbbaa07f9
Soe Thura <thixpin@gmail.com>
2022-08-21 19:42:21 +0630

    Add recommand books and comments in book detail

--- plugins/thixpin/book/components/BookDetail.php
+++ plugins/thixpin/book/components/BookDetail.php
@@ -8,6 +8,7 @@ use Cms\Classes\ComponentBase;
  */
 class BookDetail extends ComponentBase
 {
+    protected $_book;
     public function componentDetails()
     {
         return [
@@ -30,12 +31,37 @@ class BookDetail extends ComponentBase
 
     public function onRun()
     {
-        $this->page['book'] = $this->book();
+        $this->page['book'] = $this->_book = $this->book();
+        $this->page['relatedBooks'] = $this->relatedBooks();
     }
 
     protected function book()
     {
         // die($this->param('slug'));
         return Book::where('slug',  $this->param('slug'))->first();
+    }
+
+    protected function relatedBooks()
+    {
+        $size = 2;
+        $book = $this->_book;
+        $categoriyIds = $book->categories()->lists('id');
+        $idsInSameCategory = Book::wherehas('categories', function ($query) use ($categoriyIds) {
+            $query->whereIn('id', $categoriyIds);
+        })->where('id', '!=', $book->id)->lists('id');
+
+        $tagIds = $book->tags()->lists('id');
+        $idsInSameTag = Book::wherehas('tags', function ($query) use ($tagIds) {
+            $query->whereIn('id', $tagIds);
+        })->where('id', '!=', $book->id)->lists('id');
+        $idsInSameAuthor = Book::where('author_id', $book->author_id)->where('id', '!=', $book->id)->lists('id');
+        $idsInSameArtist = Book::where('artist_id', $book->artist_id)->where('id', '!=', $book->id)->lists('id');
+
+        $idsWithScore = array_count_values(array_merge($idsInSameCategory, $idsInSameTag, $idsInSameAuthor, $idsInSameArtist));
+        arsort($idsWithScore);
+        $take = count($idsWithScore) > $size ? $size : count($idsWithScore);
+        $ids = array_keys(array_slice($idsWithScore, 0, $take, true));
+
+        return Book::whereIn('id', $ids)->get();
     }
 }
